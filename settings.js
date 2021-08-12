df127d25c644dd9a78071a034216a0aae53eb57b
Erich P. Lotto <erichlotto@users.noreply.github.com>
2021-08-12 17:55:27 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,16 @@
 module.exports = {
-    password: "pa55word",   // MyAlgoWallet POLICY: AT LEAST ONE NUMBER, AT LEAST ONE CHARACTER, MINIMUM LENGTH IS 8
-    headless: true,         // HIDE (true) OR DISPLAY (false) THE BROWSER WINDOW
-    debug: false           // IF DEBUG IS TRUE, NO TRANSACTIONS WILL BE MADE
+    // MyAlgoWallet POLICY: AT LEAST ONE NUMBER, AT LEAST ONE CHARACTER, MINIMUM LENGTH IS 8
+    password: "pa55word",
+
+    // HIDE (true) OR DISPLAY (false) THE BROWSER WINDOW
+    headless: true,
+    
+    // IF DEBUG IS TRUE, NO TRANSACTIONS WILL BE MADE
+    debug: false,
+
+    // (optional) your telegram bot token, create bot: https://t.me/BotFather
+    telegram_api: null,
+
+    // (optional) telegram chat id for notify, get the id: https://t.me/get_id_bot
+    telegram_chatid: null
 }
